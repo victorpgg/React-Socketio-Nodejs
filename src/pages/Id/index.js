d11fbcbd9ffd264750c5690ec449f5e588af465b
victorpgg <pinheirvictor@gmail.com>
2021-06-16 21:54:26 -0300

    adicionando google-charts proxima att retirarei o d3

--- src/pages/Id/index.js
+++ src/pages/Id/index.js
@@ -1,21 +1,38 @@
-import React, {Component} from 'react';
-import BarChart from './BarChart';
+import React from 'react';
+import Chart from "react-google-charts"
+import './styles.css';
 
-class Id extends Component {
-  
-    state = {
-      data: [1, 2, 3, 4, 5, 6],
-      width: 700,
-      height: 500,
-    }
-  
-    render() {
-      return (
-        <div className="App">
-          <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />
-        </div>
-      );
-    }
+export default function Id(){
+  return(<Chart
+    width={'1200x'}
+    height={'800px'}
+    chartType="LineChart"
+    loader={<div>Loading Chart</div>}
+    data={[
+      ['', ''],
+      [0, 0],
+      [1, 10],
+      [2, 23],
+      [3, 17],
+      [4, 18],
+      [5, 9],
+      [6, 11],
+      [7, 27],
+      [8, 33],
+      [9, 40],
+      [10, 32],
+      [11, 35],
+    ]}
+    options={{
+      curveType:'function',
+      hAxis: {
+        title: 'Time',
+      },
+      vAxis: {
+        title: 'Temperature',
+      },
+    }}
+    rootProps={{ 'data-testid': '1' }}
+  />  )
+
 }
-
-export default Id;
