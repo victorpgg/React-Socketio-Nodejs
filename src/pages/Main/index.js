728fdc1d1c9762c6035897b0a01e2385a250db72
flavia-thurler <flavia.thurler@gmail.com>
2021-07-01 22:21:01 -0300

    WIP: Estilização fronted

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -41,25 +41,37 @@ export default function Main(){
 
     return(
         <div class = "main-container">
-            <section class ="form">
+            <header>
                 <img src={logo} alt = "logo" />
-                <p>Connected: { '' + isConnected }</p>
-                <p>Temperatura: {temperature}</p>
-                <p>Flag: {'' + flag }</p>
-                <form onSubmit={handleId}>
-                    <h1>Digite o asset number:</h1>
-                    <input 
-                        placeholder="Número da peça"
-                        value={id}
-                        onChange = { e => setId(e.target.value)}
-                    />
-                    <button class="submit">Validar</button>
-                    <Link to="/search">
-                        <FiSearch size={16}  color= "#4682B4" />
-                        Pesquisar dados
-                    </Link> 
-                </form>
-            </section>
+                <div>
+                    <span>Sistema Supervisório para Sistema de Aquecimento de Fluidos Líquidos</span>
+                    <p>Trabalho de Conclusão de Curso</p>
+                    <p>Instituto Federal Fluminense - Campus Macaé</p>
+                    <p>Alunos: Flávia de Almeida Araujo e Galvone não sei seu nome todo</p>
+                </div>
+            </header>   
+            <div class = "container">
+                <div class = "div-a">
+                    <span>Temperatura</span>
+                    <div>
+                        <p>95ºC</p>
+                    </div>
+                </div>
+                <div class = "div-b">
+                    <span>Aquecimento</span>
+                    <p>Ocupar espaço</p>
+                    <p>Ocupar espaço</p>
+                    <p>Ocupar espaço</p>
+                    <p>Ocupar espaço</p>
+                    <p>Ocupar espaço</p>
+                </div>
+                <div class = "div-c">
+                    <span>Consultar Gráficos</span>
+                    <p>Connected: { '' + isConnected }</p>
+                    <p>Temperatura: {temperature}</p>
+                    <p>Flag: {'' + flag }</p>
+                </div>
+            </div>         
         </div>
         );
 }
